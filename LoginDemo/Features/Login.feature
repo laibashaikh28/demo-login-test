099bae8aca7b77e684776cac72a03ecdb8d4c0b6
Laiba Ovais <laiba.ovais@matrix.com.pk>
2024-03-21 12:45:43 +0500

    first commit

---
+++ LoginDemo/Features/Login.feature
@@ -0,0 +1,56 @@
+﻿Feature: Login
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](LoginDemo/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Successful login with valid credentials
+	Given user is at the Login page
+	When user enters '<userName>' and '<password>'
+	And user clicks the login button
+	Then the page should navigate to profile page
+
+Examples: 
+| userName   | password |
+| testuser_1 | Test@123 |
+| testuser   | Test@123 |
+| laiba123   |Laiba@123|
+
+Scenario: Failed login when both fields are empty
+	Given user is at the Login page
+	When user enters '' and ''
+	And user clicks the login button
+	Then the username field should show error
+	And the password field should show error
+
+Scenario: Failed login when username is empty and password is correct
+	Given user is at the Login page
+	When user enters '' and 'Test@123'
+	And user clicks the login button
+	Then the username field should show error
+
+Scenario: Failed login when username is correct and password is empty
+	Given user is at the Login page
+	When user enters 'testuser_1' and ''
+	And user clicks the login button
+	Then the password field should show error
+
+Scenario: Failed login when username is incorrect and password is correct
+	Given user is at the Login page
+	When user enters 'laiba' and 'Test@123'
+	And user clicks the login button
+	Then the page should display an error message
+
+Scenario: Failed login when username is correct and password is incorrect
+	Given user is at the Login page
+	When user enters 'testuser_1' and 'test123'
+	And user clicks the login button
+	Then the page should display an error message
+
+Scenario: Failed login when both fields are invalid
+	Given user is at the Login page
+	When user enters 'laiba' and 'Test123'
+	And user clicks the login button
+	Then the page should display an error message
---
+++ LoginDemo/Steps/LoginStepDefinitions.cs
@@ -0,0 +1,92 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+using Xunit;
+
+namespace LoginDemo.Steps
+{
+    [Binding]
+    public sealed class LoginStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+        private readonly ScenarioContext _scenarioContext;
+        public IWebDriver driver;
+        public LoginStepDefinitions(ScenarioContext scenarioContext, ISpecFlowOutputHelper outputHelper)
+        {
+            _specFlowOutputHelper = outputHelper;
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"user is at the Login page")]
+        public void GivenUserIsAtTheLoginPage()
+        {
+
+            driver = new ChromeDriver();
+            //Launch the Online Store Website
+            driver.Url = "https://demoqa.com/login";
+        }
+
+        [When(@"user enters '(.*)' and '(.*)'")]
+        public void WhenUserEntersAnd(string userName, string password)
+        {
+            driver.FindElement(By.Id("userName")).SendKeys(userName);
+
+            driver.FindElement(By.Id("password")).SendKeys(password);
+        }
+
+       
+        [When(@"user clicks the login button")]
+        public void WhenUserClicksTheLoginButton()
+        {
+            driver.FindElement(By.Id("login")).Click();
+        }
+        
+        [Then(@"the page should navigate to profile page")]
+        public void ThenThePageShouldNavigateToProfilePage()
+        {
+            Thread.Sleep(1000);
+
+            _specFlowOutputHelper.WriteLine(driver.Url);
+            // Assert.True(driver.FindElement(By.XPath(".//div[@class='text-right col-md-5 col-sm-12']//button[@id='submit']")).Displayed);
+            Assert.True(driver.Url == "https://demoqa.com/profile");
+            driver.Quit();
+
+        }
+
+        [Then(@"the username field should show error")]
+        public void ThenTheUsernameFieldShouldShowError()
+        {
+            Thread.Sleep(200);
+
+            _specFlowOutputHelper.WriteLine(driver.FindElement(By.Id("userName")).GetCssValue("border-color"));
+            Assert.True(driver.FindElement(By.Id("userName")).GetCssValue("border-color") == "rgb(220, 53, 69)");
+            
+            //String color = element.getCSSValue;
+        }
+     
+
+        [Then(@"the password field should show error")]
+        public void ThenThePasswordFieldShouldShowError()
+        {
+            Thread.Sleep(200);
+
+            _specFlowOutputHelper.WriteLine(driver.FindElement(By.Id("password")).GetCssValue("border-color"));
+            Assert.True(driver.FindElement(By.Id("password")).GetCssValue("border-color") == "rgb(220, 53, 69)");
+            driver.Quit();
+        }
+        [Then(@"the page should display an error message")]
+        public void ThenThePageShouldDisplayAnErrorMessage()
+        {
+            Thread.Sleep(500);
+            Assert.True(driver.FindElement(By.Id("name")).Displayed);
+            driver.Quit();
+        }
+
+
+    }
+}
