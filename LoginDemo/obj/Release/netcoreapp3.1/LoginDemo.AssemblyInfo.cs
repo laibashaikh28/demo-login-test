6c2ed82a99c9a4ca537f7f074c61fc7b3a408acf
laibashaikh28 <runner@fv-az1543-208.nfhami3213qelic5zgc4gn5ygc.cx.internal.cloudapp.net>
2024-03-28 06:17:30 +0000

    commit message

--- LoginDemo/obj/Release/netcoreapp3.1/LoginDemo.AssemblyInfo.cs
+++ LoginDemo/obj/Release/netcoreapp3.1/LoginDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6e6a4c81fce3ce0ac6a11597972e18c0719603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f4a41dbad2d8a7454832a21f75cf0d4e80ba05")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
